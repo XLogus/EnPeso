ee02dc9a21ae4ad88eb8bdef8f407bd029bbdb4c
XLogus <hiteck3000@hotmail.com>
2014-09-02 07:28:01 -0500

    Primera version
    Primeros cambios

---
+++ js/app.js
@@ -0,0 +1,146 @@
+'use strict';
+var $url = 'http://enpeso.com/panel/json/';
+
+// Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp', [
+  'ngRoute',
+  'ngCookies',
+  'ui.chart',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives',
+  'myApp.controllers'
+]);
+var app = myApp.value('charting', {
+      pieChartOptions: {
+        seriesDefaults: {
+          // Make this a pie chart.
+          renderer: jQuery.jqplot.PieRenderer,
+          rendererOptions: {
+            // Put data labels on the pie slices.
+            // By default, labels show the percentage of the slice.
+            showDataLabels: true
+          }
+        },
+        legend: { show:true, location: 'e' }
+      }
+    });
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {templateUrl: 'partials/login.html', title:'login', controller: 'ctrl_login'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', title:'home', controller: 'ctrl_home'});
+  $routeProvider.when('/dietas', {templateUrl: 'partials/dietas.html', title:'dietas', controller: 'ctrl_dietas'});
+  $routeProvider.when('/citas', {templateUrl: 'partials/citas.html', title:'citas', controller: 'ctrl_citas'});
+  $routeProvider.when('/chat', {templateUrl: 'partials/chat.html', title:'chat', controller: 'ctrl_chat'});  
+  $routeProvider.when('/registro', {templateUrl: 'partials/registro.html', title:'registro', controller: 'ctrl_registro'});
+  $routeProvider.otherwise({redirectTo: '/login'});
+}]);
+myApp.run(function($rootScope, auth) {
+    $rootScope.salir = function() {
+        auth.logout();
+    };
+    $rootScope.showmenu = function() {
+        auth.showmenu();
+    };
+});
+
+myApp.run(['$location', '$rootScope', 'auth', function($location, $rootScope, auth) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+        if($rootScope.title=="home") {
+            $rootScope.title="perfil"
+        }
+        $rootScope.activePath = $location.path();
+    });    
+    //al cambiar de rutas
+    $rootScope.$on('$routeChangeStart', function(event,current) {   
+        var activo = $location.path()
+        auth.checkStatus(activo);
+    })
+}]);
+
+//// logins
+app.factory("auth", function($cookies,$cookieStore,$location,$http,$rootScope)
+{
+    return{
+        login : function(username, password)
+        {
+            var url = $url+'login.php?callback=JSON_CALLBACK';
+            var items;
+            var rpta="dddd";
+            $cookieStore.remove("username"),
+            $cookieStore.remove("id_user");
+            $http({
+                method: "JSONP", 
+                url: url,
+                params: { 'username': username, 'password':password }
+                }).
+                success(function(data, status) {
+                    console.log(status);
+                    //$scope.data = data;
+                    items = data.items;
+                    if(items.success=="1") {
+                        $cookies.id_user = items.id_usuario;
+                        $cookies.username = items.nombre+" "+items.apellido;
+                        $location.path("/home");                     
+                    } else {
+                        $rootScope.error = 1;                                                
+                    }                   
+                }).
+                error(function(data, status) {
+                    console.log(status+"Request Failed");
+                    $rootScope.error = 1;
+                });
+                
+            
+            //creamos la cookie con el nombre que nos han pasado
+            
+                       
+            //mandamos a la home
+            //$location.path("/home");
+        },        
+        logout : function()
+        {
+            //al hacer logout eliminamos la cookie con $cookieStore.remove
+            $cookieStore.remove("username"),
+            $cookieStore.remove("id_user");
+            //mandamos al login            
+            $location.path("/login");
+        },
+        checkStatus : function(activo)
+        {
+            //creamos un array con las rutas que queremos controlar
+            var rutasPrivadas = ["/home","/dietas","/citas","/chat","/login"];
+            if(this.in_array($location.path(),rutasPrivadas) && typeof($cookies.id_user) == "undefined")
+            {
+                $location.path("/login");
+            }
+            //en el caso de que intente acceder al login y ya haya iniciado sesión lo mandamos a la home
+            if(activo=="/login" && typeof($cookies.id_user) != "undefined")
+            {
+                $location.path("/home");
+            }
+        },
+        showmenu: function() {
+            if($rootScope.activo==undefined) {
+                $rootScope.activo = 0;                
+            }            
+            if($rootScope.activo == 1) {
+                $rootScope.activo = 0;
+            } else {
+                $rootScope.activo = 1;
+            }
+        },
+        in_array : function(needle, haystack)
+        {
+            var key = '';
+            for(key in haystack)
+            {
+                if(haystack[key] == needle)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+});
---
+++ js/controllers.js
@@ -0,0 +1,78 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('ctrl_login', ['$scope', 'auth', '$location', function($scope,auth,$location) {
+        $scope.rpta = '';
+        $scope.login = function() {
+            auth.login($scope.username, $scope.password);
+        }
+  }])
+  .controller('ctrl_home', ['$scope', 'charting', '$cookies', '$http', function($scope,charting,$cookies,$http) {
+        $scope.usuario_nombre = $cookies.username;        
+        $scope.someData = [[
+        ['Heavy Industry', 12],['Retail', 9], ['Light Industry', 14],
+        ['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+        ]];
+      $scope.myChartOpts = charting.pieChartOptions;  
+    // Consultamos los datos
+    var items;
+    $http({
+                method: "JSONP", 
+                url: $url + "home.php?callback=JSON_CALLBACK"
+                }).
+                success(function(data, status) {
+                    items = data.items;
+                    $scope.peso_perdido = items.peso_perdido;
+                    $scope.cita_fecha = items.cita_fecha;
+                    $scope.cita_hora = items.cita_hora;                    
+                }).
+                error(function(data, status) {
+                    console.log(status+"Request Failed");
+                    $scope.error = 1;
+                });
+        
+      
+  }])
+  .controller('ctrl_dietas', ['$scope', '$http', function($scope, $http) {
+    var items;
+    $http({
+                method: "JSONP", 
+                url: $url + "planes.php?callback=JSON_CALLBACK"
+                }).
+                success(function(data, status) {
+                    items = data.items;
+                    $scope.peso_perdido = items.peso_perdido;
+                    $scope.cita_fecha = items.cita_fecha;
+                    $scope.cita_hora = items.cita_hora;                    
+                }).
+                error(function(data, status) {
+                    console.log(status+"Request Failed");
+                    $scope.error = 1;
+                });
+  }])
+  .controller('ctrl_citas', ['$scope', '$http', function($scope, $http) {
+    var items;
+    $http({
+                method: "JSONP", 
+                url: $url + "consulta.php?callback=JSON_CALLBACK"
+                }).
+                success(function(data, status) {
+                    items = data.items;                    
+                    $scope.cita_fecha = items.cita_fecha;
+                    $scope.cita_hora = items.cita_hora;                    
+                }).
+                error(function(data, status) {
+                    console.log(status+"Request Failed");
+                    $scope.error = 1;
+                });
+
+  }])
+  .controller('ctrl_registro', ['$scope', function($scope) {
+
+  }])
+  .controller('ctrl_chat', ['$scope', function($scope) {
+
+  }]);
+  
